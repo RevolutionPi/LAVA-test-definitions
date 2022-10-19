630b0d758c32aa82552404cd9a2af2b28da86272
Ramiro Gsponer <r.gsponer@kunbus.com>
2022-10-19 10:49:21 +0200

    Update test for LEDs
    Test modified.
    Command tree has been removed from test.
    
    Signed-off-by: Ramiro Gsponer <r.gsponer@kunbus.com>

--- automated/linux/leds/test-leds1.sh
+++ automated/linux/leds/test-leds1.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 TEST_CASE_NAME=$(basename "$0" .sh)
 
-declare -a LEDS_CORE_TREE=(
-	"├── a1_green -> ../../devices/platform/leds/leds/a1_green"
-	"├── a1_red -> ../../devices/platform/leds/leds/a1_red"
-	"├── a2_green -> ../../devices/platform/leds/leds/a2_green"
-	"├── a2_red -> ../../devices/platform/leds/leds/a2_red"
-	"├── default-on -> ../../devices/virtual/leds/default-on"
-	"├── mmc0 -> ../../devices/virtual/leds/mmc0"
-	"└── power_red -> ../../devices/platform/leds/leds/power_red"
+declare -a LEDS_CORE=(
+	"/sys/class/leds/a1_green"
+	"/sys/class/leds/a1_red"
+	"/sys/class/leds/a2_green"
+	"/sys/class/leds/a2_red"
+	"/sys/class/leds/default-on"
+	"/sys/class/leds/mmc0"
+	"/sys/class/leds/power_red"
 )
 
-RET_TREE=$(tree /sys/class/leds)
-for i in "${LEDS_CORE_TREE[@]}"
+for i in "${LEDS_CORE[@]}"
 do
-	if echo "$RET_TREE" | grep -o "$i";
+	if [[ -d "$i" ]];
 	then
 		lava-test-case "$TEST_CASE_NAME-$i" --result pass
 	else
