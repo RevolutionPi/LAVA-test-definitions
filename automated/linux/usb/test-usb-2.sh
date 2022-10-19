833da014650aff18e2e1b248d1ab37a2ba118fe0
Ramiro Gsponer <r.gsponer@kunbus.com>
2022-10-19 14:13:28 +0200

    Add RevPi Connect in USB tests
    Signed-off-by: Ramiro Gsponer <r.gsponer@kunbus.com>

--- automated/linux/usb/test-usb-2.sh
+++ automated/linux/usb/test-usb-2.sh
@@ -4,17 +4,17 @@ TEST_CASE_NAME=$(basename "$0" .sh)
 PATH_DEV="/dev/sda1"
 PATH_MOUNT="/mnt/lava_usb_test"
 
-if lsblk | grep sda1;
+if lsblk | grep sda1
 then
     #Flash disk will be formatted
-    if mke2fs -F -t ext4 "$PATH_DEV";
+    if mke2fs -F -t ext4 "$PATH_DEV"
     then
         if [ ! -d "$PATH_MOUNT" ]
         then
             mkdir "$PATH_MOUNT"
         fi
 
-        if mount "$PATH_DEV" "$PATH_MOUNT";
+        if mount "$PATH_DEV" "$PATH_MOUNT"
         then
             dd if=/dev/urandom of=/tmp/testfile bs=512 count=1k
             md5sum /tmp/testfile > md5
@@ -22,7 +22,7 @@ then
             cp /tmp/testfile "$PATH_MOUNT"
             cp md5 "$PATH_MOUNT"
             
-            if cd "$PATH_MOUNT" && md5sum -c md5;
+            if cd "$PATH_MOUNT" && md5sum -c md5
             then
                 lava-test-case "$TEST_CASE_NAME-md5sum" --result pass
             else
