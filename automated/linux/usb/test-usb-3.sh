12f00dae21c516db0b321c765079c334aaaa8bfe
Ramiro Gsponer <r.gsponer@kunbus.com>
2022-10-19 14:13:28 +0200

    Add RevPi Connect in USB tests
    Signed-off-by: Ramiro Gsponer <r.gsponer@kunbus.com>

--- automated/linux/usb/test-usb-3.sh
+++ automated/linux/usb/test-usb-3.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 TEST_CASE_NAME=$(basename "$0" .sh)
 
-dd if=/dev/sda1 of=/dev/null status=progress conv=sync iflag=nocache oflag=nocache bs=1k count=1M
-RET=$?
-
-if [ $RET -eq 0 ]
+if dd if=/dev/sda1 of=/dev/null status=progress conv=sync iflag=nocache oflag=nocache bs=1k count=1M
 then
     lava-test-case "$TEST_CASE_NAME" --result pass
 else
--- automated/linux/usb/test-usb-4.sh
+++ automated/linux/usb/test-usb-4.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 TEST_CASE_NAME=$(basename "$0" .sh)
 
-dd if=/dev/zero of=/dev/sda1 status=progress conv=sync iflag=nocache oflag=nocache bs=1k count=1M
-RET=$?
-
-if [ $RET -eq 0 ]
+if dd if=/dev/zero of=/dev/sda1 status=progress conv=sync iflag=nocache oflag=nocache bs=1k count=1M
 then
     lava-test-case "$TEST_CASE_NAME" --result pass
 else
