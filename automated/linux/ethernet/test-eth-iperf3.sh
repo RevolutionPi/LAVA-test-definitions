e0c5be46f8a1cefa18bb3358fd94429559f955a2
Ramiro Gsponer <r.gsponer@kunbus.com>
2022-10-19 23:55:06 +0200

    Add RevPi Connect in ETH tests
    Signed-off-by: Ramiro Gsponer <r.gsponer@kunbus.com>

--- automated/linux/ethernet/test-eth-iperf3.sh
+++ automated/linux/ethernet/test-eth-iperf3.sh
@@ -2,19 +2,21 @@
 TEST_CASE_NAME=$(basename "$0" .sh)
 
 #Worker Host IP_ADDRESS!
-IP_ATE="192.168.168.73"
+IP_ATE="$1"
+
+apt update
+apt -q -y install iperf3
 
 ip a show eth0 | grep inet
 
-echo "Connecting with ATE IP-Address: $IP_ATE"
-if iperf3 -t 1800 -4 -c $IP_ATE -t 10;
+if iperf3 -t 1800 -4 -c "$IP_ATE" -t 10
 then
     lava-test-case "$TEST_CASE_NAME-iperf3-1/2" --result pass
 else
     lava-test-case "$TEST_CASE_NAME-iperf3-1/2" --result fail
 fi
 
-if iperf3 -t 1800 -4 -R -c $IP_ATE -t 10;
+if iperf3 -t 1800 -4 -R -c "$IP_ATE" -t 10
 then
     lava-test-case "$TEST_CASE_NAME-iperf3-2/2" --result pass
 else
