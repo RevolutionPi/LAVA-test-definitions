d376d4f6284262b28609a32c699ffe84c10a2779
Ramiro Gsponer <r.gsponer@kunbus.com>
2022-10-20 10:33:41 +0200

    Add RevPi Connect in LEDs tests
    Signed-off-by: Ramiro Gsponer <r.gsponer@kunbus.com>

--- automated/linux/leds/test-leds1.sh
+++ automated/linux/leds/test-leds1.sh
@@ -1,22 +1,66 @@
 #!/bin/bash
 TEST_CASE_NAME=$(basename "$0" .sh)
 
+#LED_TIME: LED on/off time.
+LED_TIME=1
+
+LED_BASE="/sys/class/leds"
+
 declare -a LEDS_CORE=(
-	"/sys/class/leds/a1_green"
-	"/sys/class/leds/a1_red"
-	"/sys/class/leds/a2_green"
-	"/sys/class/leds/a2_red"
-	"/sys/class/leds/default-on"
-	"/sys/class/leds/mmc0"
-	"/sys/class/leds/power_red"
+	"$LED_BASE/a1_green"
+	"$LED_BASE/a1_red"
+	"$LED_BASE/a2_green"
+	"$LED_BASE/a2_red"
+	"$LED_BASE/default-on"
+	"$LED_BASE/mmc0"
+	"$LED_BASE/power_red"
 )
 
-for i in "${LEDS_CORE[@]}"
+declare -a LEDS_CONNECT=(
+	"$LED_BASE/a1_green"
+	"$LED_BASE/a1_red"
+	"$LED_BASE/a2_green"
+	"$LED_BASE/a2_red"
+	"$LED_BASE/a3_green"
+	"$LED_BASE/a3_red"
+	"$LED_BASE/default-on"
+	"$LED_BASE/leds/mmc0"
+	"$LED_BASE/power_red"
+)
+
+case "$1" in
+"Connect")
+	LEDS=( "${LEDS_CONNECT[@]}" )
+	;;
+"Core")
+	LEDS=( "${LEDS_CORE[@]}" )
+	;;
+Default)
+	;;
+esac
+
+for i in "${LEDS[@]}"
 do
-	if [[ -d "$i" ]];
+	if [[ -d "$i" ]]
 	then
 		lava-test-case "$TEST_CASE_NAME-$i" --result pass
 	else
 		lava-test-case "$TEST_CASE_NAME-$i" --result fail
 	fi
+
+	if echo 1 > "$i"/brightness
+	then
+		lava-test-case "$TEST_CASE_NAME-$i/brightness-green" --result pass
+	else
+		lava-test-case "$TEST_CASE_NAME-$i/brightness-green" --result fail
+	fi
+	
+	sleep $LED_TIME
+	if echo 0 > "$i"/brightness
+	then
+		lava-test-case "$TEST_CASE_NAME-$i/brightness-red" --result pass
+	else
+		lava-test-case "$TEST_CASE_NAME-$i/brightness-red" --result fail
+	fi
+	# TODO: This test should be modified for external hardware...
 done
