27815c01a1335056811700f7d376bcdeff9169cd
Ramiro Gsponer <r.gsponer@kunbus.com>
2023-07-03 13:03:11 +0200

    fix: Separate checks into individual tests
    Refactored the health check script to perform individual checks for CPU
    load, available memory, and disk space. Each health check is now
    handled separately, allowing for better reporting and identification
    of failures.
    
    The script now includes separate if statements for each health check,
    ensuring that a specific test is marked as a failure when its
    corresponding check fails. The lava-test-case command is used to report
    the results accurately.
    
    The commit also includes minor improvements, such as adding descriptive
    test case names and appropriate failure messages when a health check
    fails.
    
    Signed-off-by: Ramiro Gsponer <r.gsponer@kunbus.com>

--- automated/health-checks/tests/health-check.sh
+++ automated/health-checks/tests/health-check.sh
@@ -3,14 +3,35 @@ TEST_CASE_NAME=$(basename "$0" .sh)
 
 # Check CPU load
 cpu_load=$(uptime | awk -F 'load average:' '{print $2}' | awk -F, '{print $1}' | awk '{$1=$1};1')
-echo "CPU Load: $cpu_load"
+
+# Check if CPU load check failed
+if [[ -z $cpu_load ]]; then
+  lava-test-case "$TEST_CASE_NAME-cpu-load" --result fail
+else
+  echo "CPU Load: $cpu_load"
+  lava-test-case "$TEST_CASE_NAME-cpu-load" --result pass
+fi
+
 
 # Check available memory
 free_mem=$(free -m | awk 'NR==2{printf "%.2f%%", $3*100/$2}')
-echo "Available Memory: $free_mem"
+
+# Check if available memory check failed
+if [[ -z $free_mem ]]; then
+  lava-test-case "$TEST_CASE_NAME-available-memory" --result fail
+else
+  echo "Available Memory: $free_mem"
+  lava-test-case "$TEST_CASE_NAME-available-memory" --result pass
+fi
+
 
 # Check disk space
 disk_space=$(df -h / | awk 'NR==2{printf "%s", $5}')
-echo "Disk Space: $disk_space"
 
-lava-test-case "$TEST_CASE_NAME-Health-Check" --result pass
+# Check if disk space check failed
+if [[ -z $disk_space ]]; then
+  lava-test-case "$TEST_CASE_NAME-disk-space" --result fail
+else
+  echo "Disk Space: $disk_space"
+  lava-test-case "$TEST_CASE_NAME-disk-space" --result pass
+fi
