578306687c87968d5014fbeabe74230e8c98ffd2
Thomas Böhler <t.boehler@kunbus.com>
2023-07-10 13:39:53 +0200

    refactor: move config_loader to device-config folder
    Signed-off-by: Ramiro Gsponer <r.gsponer@kunbus.com>
    Signed-off-by: Thomas Böhler <t.boehler@kunbus.com>

---
+++ automated/device-config/config_loader.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+TEST_CASE_NAME=$(basename "$0" .sh)
+
+CONFIG_PATH="/var/www/revpi/pictory/projects"
+
+CONFIG="$1"
+
+if ! cp "$CONFIG" "$CONFIG_PATH"/_config.rsc
+then
+    # fail the complete job if setting up the configuration fails
+    # if this fails the following jobs most likely fail, too
+    lava-test-raise "$TEST_CASE_NAME-install-config"
+else
+    lava-test-case "$TEST_CASE_NAME-install-config" --result pass
+fi
+
+chown www-data:www-data "$CONFIG_PATH"/_config.rsc
+
+if ! piTest -x
+then
+    lava-teset-raise "$TEST_CASE_NAME-reload-driver"
+else
+    lava-test-case "$TEST_CASE_NAME-reload-driver" --result pass
+fi
+
+# wait for piTest configuration to be loaded
+sleep 1
