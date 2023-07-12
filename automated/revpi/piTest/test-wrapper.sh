ae9a4099513067954701690c965dc054bbabbe68
Ramiro Gsponer <r.gsponer@kunbus.com>
2023-07-12 22:14:22 +0200

    feat: Add some tests for piTest - Compact
    Signed-off-by: Ramiro Gsponer <r.gsponer@kunbus.com>

---
+++ automated/revpi/piTest/test-wrapper.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source ../../../lib/piTest.sh
+
+test-pt-compact-d-1() {
+    piTest_Check_001 "test-compact-pt" "DI1" "DO1"
+    piTest_Check_001 "test-compact-pt" "DI2" "DO2"
+}
+
+test-pt-compact-a-1() {
+    piTest_Check_002 "test-compact-analog-01" "AI2" "AO1"
+    piTest_Check_002 "test-compact-analog-01" "AI1" "AO2"
+}
+
+if [[ $# -eq 0 ]]; then
+    echo "Usage: $0 TEST_CASE_NAME INPUT OUTPUT" >&2
+    exit 1
+fi
+
+# call given function
+$1
