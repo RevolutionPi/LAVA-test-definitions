bb1e05893c5b4c03c143172c26394f0738cf69f4
Ramiro Gsponer <r.gsponer@kunbus.com>
2023-07-13 09:16:46 +0200

    refactor: Add piTests for analog IO
    Signed-off-by: Ramiro Gsponer <r.gsponer@kunbus.com>

--- lib/piTest.sh
+++ lib/piTest.sh
@@ -4,6 +4,11 @@
 LOW=0
 HIGH=1
 
+ANALOG_START=0
+ANALOG_END=10000
+ANALOG_STEP=1000
+ANALOG_RANGE=250
+
 # Function for setting the IO value
 piTest_setIOValue() (
   test_case_name=$1
@@ -31,7 +36,7 @@ piTest_setIOValue() (
   fi
 )
 
-# Function for checking the IO value
+# Function for checking digital IO value
 piTest_validateIOValue() (
   if [ "$(piTest -v "$2")" != "Cannot read variable info" ]
   then
@@ -46,6 +51,24 @@ piTest_validateIOValue() (
   fi
 )
 
+# Function for checking analog IO value
+piTest_validateAIOValue() (
+  if [ "$(piTest -v "$2")" != "Cannot read variable info" ]
+  then
+    value=$(piTest -q -1 -r "$2")
+    range_low=$(( $3 - ANALOG_RANGE ))
+    range_high=$(( $3 + ANALOG_RANGE ))
+
+    if ! (( value >= range_low && value <= range_high ))
+    then
+      lava-test-case "$1-$2-value-$3-is-$value" --result fail
+    fi
+  else
+    lava-test-case "$1-variable-not-found-$2" --result fail
+  fi
+)
+
+# Function for digital IO
 piTest_Check_001() (
   # $1: TEST_CASE_NAME
   # $2: INPUT
@@ -65,4 +88,29 @@ piTest_Check_001() (
   # wait for process image
   sleep 1
   piTest_validateIOValue "$test_case_name" "$input" "$HIGH"
+)
+
+# Function for analog IO
+piTest_Check_002() (
+  # $1: TEST_CASE_NAME
+  # $2: INPUT
+  # $3: OUTPUT
+  # $4: VALUE
+  test_case_name=$1
+  input=$2
+  output=$3
+
+  # set output with ANALOG_VALx
+  for ((analog_value = ANALOG_START; analog_value <= ANALOG_END; analog_value += ANALOG_STEP)); do
+    piTest_setIOValue "$test_case_name" "$output" "$analog_value"
+    # Wait for process image
+    sleep 1
+    piTest_validateAIOValue "$test_case_name" "$input" "$analog_value"
+  done
+
+  # set output to zero
+  piTest_setIOValue "$test_case_name" "$output" "$LOW"
+  # wait for process image
+  sleep 1
+  piTest_validateAIOValue "$test_case_name" "$input" "$LOW"
 )
