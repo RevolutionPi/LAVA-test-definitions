1cc430a719a6101ebcef57127a29f32d4eb1f50a
Ramiro Gsponer <r.gsponer@kunbus.com>
2023-07-13 12:49:33 +0200

    feat: Add some tests for piTest - Flat
    Signed-off-by: Ramiro Gsponer <r.gsponer@kunbus.com>

--- automated/revpi/piTest/test-wrapper.sh
+++ automated/revpi/piTest/test-wrapper.sh
@@ -12,6 +12,12 @@ test-pt-compact-a-1() {
     piTest_Check_002 "test-compact-analog-01" "AI1" "AO2"
 }
 
+test-pt-flat-da-1() {
+    piTest_setIOValue "test-flat-digital" "DOut" "1"
+    piTest_Check_002 "test-flat-analog" "AIn" "AOut"
+    piTest_setIOValue "test-flat-digital" "DOut" "0"
+}
+
 if [[ $# -eq 0 ]]; then
     echo "Usage: $0 TEST_CASE_NAME INPUT OUTPUT" >&2
     exit 1
