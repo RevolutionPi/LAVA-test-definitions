d446298a7c3e03732eb6940b3899396798652e75
Ramiro Gsponer <r.gsponer@kunbus.com>
2023-08-09 12:06:11 +0200

    refactor: LAVA decoupling: Image flash
    Modified the script to operate independently from LAVA, enabling it to
    run without the LAVA infrastructure.
    Removed LAVA-specific stuff and replaced them with standard error
    handling techniques. The handling of exit codes has been integrated
    into the YAML file used to execute the test.
    
    These changes ensure that the script can be effectively used both
    within and outside of the LAVA testing framework.
    
    Signed-off-by: Ramiro Gsponer <r.gsponer@kunbus.com>

--- automated/deployment/lava_prog_device.sh
+++ automated/deployment/lava_prog_device.sh
@@ -7,51 +7,73 @@
 # This script relies on features from LAVA and can't be run on a
 # user controlled setup without changes.
 
-set +x
+# shellcheck disable=SC1091
+. ../lib/sh-test-lib
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
+export RESULT_FILE
 
-# shellcheck disable=SC2005
-absdirname () { echo "$(cd "$(dirname "$1")" && pwd)"; }
-SRC_ROOT="$(absdirname "${BASH_SOURCE[0]}")"
+usage() {
+    echo "Usage: $0 [-s <true|false>]" 1>&2
+    exit 1
+}
 
-# include echo helper
-# shellcheck disable=SC1090
-. "$SRC_ROOT/tools/echohelper.sh"
+while getopts "s:" o; do
+  case "$o" in
+    s) ;;
+    *) usage ;;
+  esac
+done
 
-TEST_NAME="image_deployment"
+prog_device() {
+    set +x
 
-# find image and extract
-cd /lava-lxc || exit
+    # shellcheck disable=SC2005
+    absdirname () { echo "$(cd "$(dirname "$1")" && pwd)"; }
+    SRC_ROOT="$(absdirname "${BASH_SOURCE[0]}")"
 
-ARCHIVE=$(find . -maxdepth 1 -iname "*.xz" -type f)
-xz --decompress "$ARCHIVE"
+    # include echo helper
+    # shellcheck disable=SC1090
+    . "$SRC_ROOT/tools/echohelper.sh"
 
-IMAGE=$(find . -maxdepth 1 -iname "*.img" -type f)
-echo "$IMAGE"
+    # find image and extract
+    cd /lava-lxc || exit
 
-echoinfo "please wait, calculating md5sum for image"
-md5_img=$(md5sum "$IMAGE" | awk '{ print $1 }' )
+    ARCHIVE=$(find . -maxdepth 1 -iname "*.xz" -type f)
+    xz --decompress "$ARCHIVE"
 
-usb_disk=$(find /sys/devices -iname "${LAVA_STATIC_INFO_0_usb_path:?}" -exec find {} -iname block -print0 \; 2>/dev/null | xargs -0 ls)
-disk=$(lsblk -I 8 -dno NAME,RM | awk '{ if  ($2 == 1) { print $1 } }')
+    IMAGE=$(find . -maxdepth 1 -iname "*.img" -type f)
+    echo "$IMAGE"
 
-if [ ! "$usb_disk" == "$disk" ]; then
-  echoerr "Blockdevice from lsblk and sysfs are different ($disk - $usb_disk)"
-  lava-test-raise "$TEST_NAME+disk-device"
-fi
+    echoinfo "please wait, calculating md5sum for image"
+    md5_img=$(md5sum "$IMAGE" | awk '{ print $1 }' )
 
-echoinfo "programming the image on storage device /dev/$disk"
-dd if="$IMAGE" of="/dev/$disk" bs=64k
-sync
+    usb_disk=$(find /sys/devices -iname "${LAVA_STATIC_INFO_0_usb_path:?}" -exec find {} -iname block -print0 \; 2>/dev/null | xargs -0 ls)
+    disk=$(lsblk -I 8 -dno NAME,RM | awk '{ if  ($2 == 1) { print $1 } }')
 
-echoinfo "verifying disk vs image"
-md5_disk=$(dd if="/dev/$disk" bs=64k count="$(stat -c %s "$IMAGE")" iflag=count_bytes status=progress | md5sum | awk '{ print $1 }' )
-echo "$md5_img"
-echo "$md5_disk"
+    if [ ! "$usb_disk" == "$disk" ]; then
+    echoerr "Blockdevice from lsblk and sysfs are different ($disk - $usb_disk)"
+    exit 1
+    fi
 
-if [[ "$md5_img" != "$md5_disk" ]]; then
-  echoerr "image on disk seems to have errors"
-  lava-test-raise "$TEST_NAME+md5sum"
-fi
+    echoinfo "programming the image on storage device /dev/$disk"
+    dd if="$IMAGE" of="/dev/$disk" bs=64k
+    sync
 
-lava-test-case "$TEST_NAME" --result pass
+    echoinfo "verifying disk vs image"
+    md5_disk=$(dd if="/dev/$disk" bs=64k count="$(stat -c %s "$IMAGE")" iflag=count_bytes status=progress | md5sum | awk '{ print $1 }' )
+    echo "$md5_img"
+    echo "$md5_disk"
 
+    if [[ "$md5_img" != "$md5_disk" ]]; then
+    echoerr "image on disk seems to have errors"
+    exit 1
+    fi
+}
+
+# Test run.
+create_out_dir "${OUTPUT}"
+
+prog_device
+
+check_return "prog_device"
