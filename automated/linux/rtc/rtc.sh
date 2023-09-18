c0936ec3dd471a4d7fb26558f8dcb1fb71fdfcbe
Ramiro Gsponer <r.gsponer@kunbus.com>
2023-09-18 06:31:45 +0200

    feat: Automate RTC tests
    RTC-1:
    Set hardware clock to the date and time specified in `DATE_SET` and
    verify the expected time.
    
    RTC-2-a:
    Disable NTP synchronization, set hardware clock to the specific date
    and time provided by `DATE_SET` variable.
    If `SKIP_REBOOT` variable is equal to "true", it skips the reboot.
    Otherwise, a reboot of the DUT will occur within 1 minute.
    After the reboot, the RTC status can be checked using the independent
    test `RTC-2-b`.
    Note that NTP will not be automatically re-enabled after a reboot.
    This must be handled by the tester in the script execution environment.
    
    RTC-2-b:
    Verify the expected time. This check is usually used after reboot of
    test RTC-2-a.
    By default this test will run with the `SKIP_REBOOT` variable set to
    "true" so at the end of the test the NTP is re-enabled again.
    
    NOTE:
    Usually if a reboot is planned (SKIP_REBOOT != "true"), it would mean
    that the RTC-2-b test will be executed after a restart.
    After a test execution, it is crucial to ensure that NTP is re-enabled
    and started to guarantee that subsequent tests have the correct time
    configuration.
    Therefore, 'timedatectl set-ntp true' is executed, but only if the DUT
    should not be restarted.
    This ensures that synchronization with the Network Time Protocol (NTP)
    is maintained to keep accurate time in subsequent tests.
    
    Signed-off-by: Ramiro Gsponer <r.gsponer@kunbus.com>

---
+++ automated/linux/rtc/rtc.sh
@@ -0,0 +1,127 @@
+#!/bin/sh
+
+# shellcheck disable=SC1091
+. ../../lib/sh-test-lib
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
+export RESULT_FILE
+TESTS="rtc-1 rtc-2a"
+DATE_SET="2023-12-01 11:11:00"
+SKIP_REBOOT="true"
+TOLERANCE_LOW_SEC=10
+TOLERANCE_DEFAULT_MIN=10
+
+usage() {
+    echo "Usage: $0 [-s <true|false>] [-r <true|false>] [-t TESTS] [-d DATE_SET]" 1>&2
+    echo "Example: $0 -s true -r false -t 'rtc-1 rtc-2a' -d '2023-12-01 11:11:00'" 1>&2
+    exit 1
+}
+
+while getopts "s:r:t:d:h" o; do
+  case "$o" in
+    s) SKIP_INSTALL="${OPTARG}" ;;
+    r) SKIP_REBOOT="${OPTARG}" ;;
+    t) TESTS="${OPTARG}" ;;
+    d) DATE_SET="${OPTARG}" ;;
+    h|*) usage ;;
+  esac
+done
+
+install() {
+    dist_name
+
+    # No dependencies to install
+}
+
+check_hwclock() {
+    # shellcheck disable=SC3043
+    EXPECTED_TIME="$1"
+    TOLERANCE_MINUTES="$2"
+    TOLERANCE_SECONDS=$((TOLERANCE_MINUTES * 60))
+
+    if [ "$TOLERANCE_SECONDS" -lt "$TOLERANCE_LOW_SEC" ]; then
+        TOLERANCE_SECONDS=$TOLERANCE_LOW_SEC
+    fi
+
+    # Convert the date strings to epoch timestamps
+    EXPECTED_TIMESTAMP=$(date -d "$EXPECTED_TIME" +%s)
+    HWCLOCK_TIMESTAMP=$(date +%s)
+
+    # Calculate the difference in seconds between expected and actual timestamps
+    TIME_DIFF=$((HWCLOCK_TIMESTAMP - EXPECTED_TIMESTAMP))
+
+    info_msg "Expected timestamp: $EXPECTED_TIMESTAMP"
+    info_msg "hwclock timestamp: $HWCLOCK_TIMESTAMP"
+    info_msg "Tolerance in seconds: $TOLERANCE_SECONDS"
+
+    # Check if the difference is within the tolerance
+    if [ "$TIME_DIFF" -lt "$TOLERANCE_SECONDS" ]; then
+        info_msg "hwclock is correct after setting."
+        return 0
+    else
+        error_msg "hwclock is not correct after setting."
+    fi
+}
+
+rtc_1() {
+    info_msg "Set hardware clock to $DATE_SET"
+    hwclock --set --date "$DATE_SET"
+    # Synchronize system time according to hardware clock
+    hwclock -s
+    # Check if hwclock is correct after setting
+    check_hwclock "$DATE_SET"
+}
+
+rtc_2a() {
+    # Disable NTP, set hardware clock to "$DATE_SET"
+    timedatectl set-ntp false
+    info_msg "Set hardware clock to $DATE_SET"
+    hwclock --set --date "$DATE_SET"
+    # Synchronize system time according to hardware clock
+    hwclock -s
+    check_hwclock "$DATE_SET"
+    # Reboot DUT if desired
+    [ "${SKIP_REBOOT}" = "true" ] || shutdown -r +1
+}
+
+rtc_2b() {
+    # Verify the expected time after reboot
+    check_hwclock "$DATE_SET" "$TOLERANCE_DEFAULT_MIN"
+}
+
+run() {
+    # shellcheck disable=SC3043
+    local test="$1"
+    test_case_id="${test}"
+    info_msg "Running ${test_case_id} test..."
+
+    case "$test" in
+        "rtc-1")
+            rtc_1
+            ;;
+        "rtc-2a")
+            rtc_2a
+            ;;
+        "rtc-2b")
+            rtc_2b
+            ;;
+    esac
+
+    check_return "${test_case_id}"
+}
+
+# Test run.
+create_out_dir "${OUTPUT}"
+
+if [ "${SKIP_INSTALL}" = "true" ] || [ "${SKIP_INSTALL}" = "True" ]; then
+    info_msg "Package installation skipped"
+else
+    install
+fi
+
+for t in $TESTS; do
+    run "$t"
+done
+
+# Enable NTP again after tests are run (only if the DUT should not reboot)
+[ "${SKIP_REBOOT}" != "true" ] || timedatectl set-ntp true
