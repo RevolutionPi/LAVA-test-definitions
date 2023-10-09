19a6f50c2baf1d7b8760c66d0942383032dec0f4
Ramiro Gsponer <r.gsponer@kunbus.com>
2023-10-09 08:27:24 +0200

    feat: Add tpm tests for RevPi devices
    This commit introduces a script for testing TPM functionality on RevPi
    devices.
    Verification of TPM functionality is performed using the `piSerial`
    and `tpm2-tools`.
    Parameters <device-serial-nr> and <revpi-password> are mandatories.
    
    Test Cases:
    - `tpm-1`: Verify that the serial number and password are correct using
    piSerial.
    - `tpm-2`: Checks for TPM properties using 'tpm2_getcap'.
    - `tpm-2b`: Alternative approach for checking TPM properties.
    
    Test `tpm-1` can be used on all RevPi devices.
    Tests `tpm-2` and `tpm-2b` can be run on RevPi Flat, RevPi Flat S and
    RevPi Connect 4 devices.
    `tpm-2` is for use with versions prior to Debian Bullseye.
    `tpm-2b` is for use starting from Debian Bullseye.
    
    Signed-off-by: Ramiro Gsponer <r.gsponer@kunbus.com>

---
+++ automated/revpi/tpm/tpm.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+. ../../lib/sh-test-lib
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
+export RESULT_FILE
+TESTS="tpm-1"
+SKIP_INSTALL="false"
+PISERIAL_SERIAL_NR=""
+PISERIAL_PASS=""
+
+usage() {
+    echo "Usage: $0 [-s <true|false>] [-t test] -S <device-serial-nr> -P <revpi-password>" 1>&2
+    exit 1
+}
+
+while getopts "s:t:S:P:h" o; do
+   case "$o" in
+    s) SKIP_INSTALL="${OPTARG}" ;;
+    t) TESTS="${OPTARG}" ;;
+    S) PISERIAL_SERIAL_NR="${OPTARG}" ;;
+    P) PISERIAL_PASS="${OPTARG}" ;;
+    h|*) usage ;;
+  esac
+done
+
+install() {
+    dist_name
+
+    apt-get update -q
+    apt-get -y install piserial tpm2-tools
+}
+
+run() {
+    local test_case_id="$1"
+    local piserial_output=""
+    info_msg "Running ${test_case_id} test..."
+
+    case "$test_case_id" in
+        "tpm-1")
+            piserial_output="$(piSerial -s)"
+            if [ "$piserial_output" != "$PISERIAL_SERIAL_NR" ]; then
+                error_msg "${test_case_id} Serial number not ok! (output: $piserial_output, actual: $PISERIAL_SERIAL_NR)"
+            fi
+
+            piserial_output="$(piSerial -p)"
+            if [ "$piserial_output" != "$PISERIAL_PASS" ]; then
+                error_msg "${test_case_id} Password not ok! (output: $piserial_output, actual: $PISERIAL_PASS)"
+            fi
+            ;;
+        "tpm-2")
+            if ! tpm2_getcap --capability="properties-fixed" ; then
+                error_msg "${test_case_id} fail!"
+            fi
+            ;;
+        "tpm-2b")
+            if ! tpm2_getcap properties-fixed ; then
+                error_msg "${test_case_id} fail!"
+            fi
+            ;;
+    esac
+
+    check_return "${test_case_id}"
+}
+
+# Test run
+create_out_dir "${OUTPUT}"
+
+if [ -z "$PISERIAL_SERIAL_NR" ] || [ -z "$PISERIAL_PASS" ]; then
+  echo "Options -S and -P are mandatory." >&2
+  usage
+fi
+
+if [ "${SKIP_INSTALL}" = "true" ] || [ "${SKIP_INSTALL}" = "True" ]; then
+    info_msg "Package installation skipped"
+else
+    install
+fi
+
+for t in $TESTS; do
+    run "$t"
+done
