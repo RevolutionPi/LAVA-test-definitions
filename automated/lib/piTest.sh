37611889467d5623a5749eaede372ebaf56e27c3
Ramiro Gsponer <r.gsponer@kunbus.com>
2023-11-25 23:31:55 +0100

    refactor: Enhance existing test for piTest
    This commit refactors an existing test script to improve its
    functionality with the output of the "piTest -d" command.
    
    If a module is not present, not configured or needs to be updated, a
    lava-test-case will fail.
    
    Signed-off-by: Ramiro Gsponer <r.gsponer@kunbus.com>

--- automated/lib/piTest.sh
+++ automated/lib/piTest.sh
@@ -9,108 +9,162 @@ ANALOG_END=10000
 ANALOG_STEP=1000
 ANALOG_RANGE=250
 
+# Function to check if a module is NOT configured
+is_module_configured() {
+    info_msg "$1" | grep -q "but NOT CONFIGURED"
+}
+
+# Function to check if a module is not physically present
+is_module_not_present() {
+    info_msg "$1" | grep -q "Module is NOT present"
+}
+
+# Function to check if a update for modules is needed
+is_module_updated() {
+    info_msg "$1" | grep -q "The firmware of some I/O modules must be updated."
+}
+
+# Function to check hardware setup configured in pictory.
+piTest_Check_config() (
+    # $1: TEST_CASE_NAME
+    # $2: PIT_TEST_OUTPUT
+    test_case_name=$1
+    pi_test_output=$2
+
+    # Check if piTest -x fails
+    if ! piTest -x
+    then
+        report_fail "$test_case_name-piTest-x-fail"
+    fi
+
+    # Check if a module is NOT configured
+    if is_module_configured "$pi_test_output"
+    then
+        report_fail "$test_case_name-HW_CONFIGURED"
+    else
+        report_pass "$test_case_name-HW_CONFIGURED"
+    fi
+
+    # Check if a module is not physically present
+    if is_module_not_present "$pi_test_output"
+    then
+        report_fail "$test_case_name-HW_NOT_PRESENT"
+    else
+        report_pass "$test_case_name-HW_NOT_PRESENT"
+    fi
+
+    # Check if an update is required
+    if is_module_updated "$pi_test_output"
+    then
+        lava-test-case "$test_case_name-HW_UPDATE" --result fail
+    fi
+)
+
 # Function for setting the IO value
 piTest_setIOValue() (
-  test_case_name=$1
-  variable=$2
-  value=$3
+    test_case_name=$1
+    variable=$2
+    value=$3
 
-  output=$(piTest -w "$variable","$value")
-  ret=$?
+    output=$(piTest -w "$variable","$value")
+    ret=$?
 
-  # XXX: hack: piTest is broken:
-  #  - no proper exit code on failure
-  #  - no usage of stderr for error messages
+    # XXX: hack: piTest is broken:
+    #  - no proper exit code on failure
+    #  - no usage of stderr for error messages
 
-  if echo "$output" | grep -E "(Cannot find variable)|(Wrong arguments)"
-  then
-    lava-test-case "$test_case_name-piTest" --result fail
+    if echo "$output" | grep -E "(Cannot find variable)|(Wrong arguments)"
+    then
+        report_fail "$test_case_name-piTest"
     return
-  fi
+    fi
 
-  # XXX: piTest never seems to return an error code
-  if [ $ret -ne 0 ]
-  then
-    lava-test-case "$test_case_name-piTest-write" --result fail
+    # XXX: piTest never seems to return an error code
+    if [ $ret -ne 0 ]
+    then
+        report_fail "$test_case_name-piTest-write"
     return
-  fi
+    fi
 )
 
 # Function for checking digital IO value
 piTest_validateIOValue() (
-  if [ "$(piTest -v "$2")" != "Cannot read variable info" ]
-  then
-    if [ "$(piTest -q -1 -r "$2")" -ne "$3" ]
+    if [ "$(piTest -v "$2")" != "Cannot read variable info" ]
     then
-      lava-test-case "$1-$2" --result fail
+        if [ "$(piTest -q -1 -r "$2")" -ne "$3" ]
+        then
+            report_fail "$1-$2"
+        else
+            report_pass "$1-$2"
+        fi
     else
-      lava-test-case "$1-$2" --result pass
+        report_fail "$1-variable-not-found-$2"
     fi
-  else
-    lava-test-case "$1-variable-not-found-$2" --result fail
-  fi
 )
 
 # Function for checking analog IO value
 piTest_validateAIOValue() (
-  if [ "$(piTest -v "$2")" != "Cannot read variable info" ]
-  then
-    value=$(piTest -q -1 -r "$2")
-    range_low=$(( $3 - ANALOG_RANGE ))
-    range_high=$(( $3 + ANALOG_RANGE ))
-
-    if ! (( value >= range_low && value <= range_high ))
+    if [ "$(piTest -v "$2")" != "Cannot read variable info" ]
     then
-      lava-test-case "$1-$2-value-$3-is-$value" --result fail
+        value=$(piTest -q -1 -r "$2")
+        range_low=$(( $3 - ANALOG_RANGE ))
+        range_high=$(( $3 + ANALOG_RANGE ))
+
+        if [ $range_low -lt 0 ]; then
+            range_low=0
+        fi
+
+        if ! [ "$value" -ge "$range_low" ] && [ "$value" -le "$range_high" ];
+        then
+            report_fail "$1-$2-value-$3-is-$value"
+        fi
+    else
+        report_fail "$1-variable-not-found-$2"
     fi
-  else
-    lava-test-case "$1-variable-not-found-$2" --result fail
-  fi
 )
 
 # Function for digital IO
 piTest_Check_001() (
-  # $1: TEST_CASE_NAME
-  # $2: INPUT
-  # $3: OUTPUT
-  test_case_name=$1
-  input=$2
-  output=$3
+    # $1: TEST_CASE_NAME
+    # $2: INPUT
+    # $3: OUTPUT
+    test_case_name=$1
+    input=$2
+    output=$3
 
-  # set output to low
-  piTest_setIOValue "$test_case_name" "$output" "$LOW"
-  # wait for process image
-  sleep 1
-  piTest_validateIOValue "$test_case_name" "$input" "$LOW"
+    # set output to low
+    piTest_setIOValue "$test_case_name" "$output" "$LOW"
+    # wait for process image
+    sleep 1
+    piTest_validateIOValue "$test_case_name" "$input" "$LOW"
 
-  # set output to high
-  piTest_setIOValue "$test_case_name" "$output" "$HIGH"
-  # wait for process image
-  sleep 1
-  piTest_validateIOValue "$test_case_name" "$input" "$HIGH"
+    # set output to high
+    piTest_setIOValue "$test_case_name" "$output" "$HIGH"
+    # wait for process image
+    sleep 1
+    piTest_validateIOValue "$test_case_name" "$input" "$HIGH"
 )
 
 # Function for analog IO
 piTest_Check_002() (
-  # $1: TEST_CASE_NAME
-  # $2: INPUT
-  # $3: OUTPUT
-  # $4: VALUE
-  test_case_name=$1
-  input=$2
-  output=$3
+    # $1: TEST_CASE_NAME
+    # $2: INPUT
+    # $3: OUTPUT
+    test_case_name=$1
+    input=$2
+    output=$3
 
-  # set output with ANALOG_VALx
-  for ((analog_value = ANALOG_START; analog_value <= ANALOG_END; analog_value += ANALOG_STEP)); do
-    piTest_setIOValue "$test_case_name" "$output" "$analog_value"
-    # Wait for process image
+    # set output with ANALOG_VALx
+    for analog_value in $(seq $ANALOG_START $ANALOG_STEP $ANALOG_END); do
+        piTest_setIOValue "$test_case_name" "$output" "$analog_value"
+        # Wait for process image
+        sleep 1
+        piTest_validateAIOValue "$test_case_name" "$input" "$analog_value"
+    done
+
+    # set output to zero
+    piTest_setIOValue "$test_case_name" "$output" "$LOW"
+    # wait for process image
     sleep 1
-    piTest_validateAIOValue "$test_case_name" "$input" "$analog_value"
-  done
-
-  # set output to zero
-  piTest_setIOValue "$test_case_name" "$output" "$LOW"
-  # wait for process image
-  sleep 1
-  piTest_validateAIOValue "$test_case_name" "$input" "$LOW"
+    piTest_validateAIOValue "$test_case_name" "$input" "$LOW"
 )
--- automated/revpi/piTest/test-wrapper.sh
+++ automated/revpi/piTest/test-wrapper.sh
@@ -1,27 +1,89 @@
-#!/bin/bash
+#!/bin/sh
 
-source ../../lib/piTest.sh
+# shellcheck disable=SC1091
+. ../../lib/sh-test-lib
+. ../../lib/piTest.sh
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
+export RESULT_FILE
 
-test-pt-compact-d-1() {
+usage() {
+    echo "Usage: $0 [-s <true|false>] [-t test]" 1>&2
+    exit 1
+}
+
+while getopts "t:s:h" o; do
+    case "$o" in
+        t) TESTS="${OPTARG}" ;;
+        s) SKIP_INSTALL="${OPTARG}" ;;
+        h|*) usage ;;
+    esac
+done
+
+install() {
+    dist_name
+
+    # No dependencies to install
+}
+
+pt_1() {
+    piTest_Check_config "test-pt1-pt2" "$(piTest -d)"
+}
+
+test_pt_compact_d_1() {
     piTest_Check_001 "test-compact-pt" "DI1" "DO1"
     piTest_Check_001 "test-compact-pt" "DI2" "DO2"
 }
 
-test-pt-compact-a-1() {
-    piTest_Check_002 "test-compact-analog-01" "AI2" "AO1"
-    piTest_Check_002 "test-compact-analog-01" "AI1" "AO2"
+test_pt_compact_a_1() {
+    piTest_Check_002 "test-compact-analog-01" "AI1" "AO1"
+    piTest_Check_002 "test-compact-analog-01" "AI2" "AO2"
 }
 
-test-pt-flat-da-1() {
+test_pt_flat_da_1() {
     piTest_setIOValue "test-flat-digital" "DOut" "1"
     piTest_Check_002 "test-flat-analog" "AIn" "AOut"
     piTest_setIOValue "test-flat-digital" "DOut" "0"
 }
 
-if [[ $# -eq 0 ]]; then
-    echo "Usage: $0 TEST_CASE_NAME INPUT OUTPUT" >&2
-    exit 1
+run() {
+    # shellcheck disable=SC3043
+    local test="$1"
+    test_case_id="${test}"
+    echo
+    info_msg "Running ${test_case_id} test..."
+
+    case "$test" in
+        "pt_1")
+            info_msg "Image test: pt-1"
+            pt_1
+            ;;
+        "test_pt_compact_d_1")
+            info_msg "Image test: test_pt_compact_d_1"
+            test_pt_compact_d_1
+            ;;
+        "test_pt_compact_a_1")
+            info_msg "Image test: test_pt_compact_a_1"
+            test_pt_compact_a_1
+            ;;
+        "test_pt_flat_da_1")
+            info_msg "Image test: test_pt_flat_da_1"
+            test_pt_flat_da_1
+            ;;
+    esac
+
+    check_return "${test_case_id}"
+}
+
+# Test run.
+create_out_dir "${OUTPUT}"
+
+if [ "${SKIP_INSTALL}" = "true" ] || [ "${SKIP_INSTALL}" = "True" ]; then
+    info_msg "Package installation skipped"
+else
+    install
 fi
 
-# call given function
-$1
+for t in $TESTS; do
+    run "$t"
+done
