5ad11bfd2f53812858fac178d7f2575bbb9da455
Ramiro Gsponer <r.gsponer@kunbus.com>
2024-01-17 15:38:33 +0100

    feat: Add tests for interfaces pileft/piright
    Signed-off-by: Ramiro Gsponer <r.gsponer@kunbus.com>

---
+++ automated/revpi/interfaces/interfaces.sh
@@ -0,0 +1,117 @@
+#!/bin/sh
+
+# shellcheck disable=SC1091
+. ../../lib/sh-test-lib
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
+export RESULT_FILE
+TESTS="pileft-1 pileft-2 dmesg"
+SKIP_INSTALL="True"
+
+usage() {
+    echo "Usage: $0 [-s <true|false>] [-t TESTS]" 1>&2
+    exit 1
+}
+
+while getopts "s:t:h" o; do
+  case "$o" in
+    s) SKIP_INSTALL="${OPTARG}" ;;
+    t) TESTS="${OPTARG}" ;;
+    h|*) usage ;;
+  esac
+done
+
+install() {
+    dist_name
+
+    # No dependencies to install
+}
+
+check_iface1() {
+    # shellcheck disable=SC3043
+    local iface_name="$1"
+    if [ -d "/sys/class/net/${iface_name}" ]; then
+        info_msg "$iface_name found"
+    else
+        error_msg "$iface_name not found!"
+    fi
+}
+
+check_iface2() {
+    # shellcheck disable=SC3043
+    local iface_name="$1"
+    if ! ip addr show "$1" ; then
+        error_msg "$iface_name not found!"
+    fi
+}
+
+check_dmesg() {
+    # shellcheck disable=SC3043
+    local dmesg_output=""
+    # shellcheck disable=SC3043
+    local set_error=0
+    dmesg_output=$(dmesg | grep -E "pileft|piright")
+    if [ -n "$dmesg_output" ]; then
+        info_msg "pileft|piright outputs in dmesg"
+        echo "$dmesg_output"
+
+        if echo "$dmesg_output" | grep "received data packet without connection"; then
+            warn_msg "received data packet without connection"
+            set_error=1
+        fi
+
+        if echo "$dmesg_output" | grep "no process image synchronization"; then
+            warn_msg "no process image synchronization"
+            set_error=1
+        fi
+
+        if echo "$dmesg_output" | grep "timeout"; then
+            warn_msg "timeout"
+            set_error=1
+        fi
+
+        if [ "$set_error" -eq 1 ]; then
+            error_msg "error(s) ocurred.. Check output of dmesg or warning messages above"
+        fi
+
+    fi
+}
+
+run() {
+    # shellcheck disable=SC3043
+    local test_case_id="$1"
+    info_msg "Running ${test_case_id} test..."
+
+    case "$test_case_id" in
+        "pileft-1")
+            check_iface1 pileft
+            ;;
+        "pileft-2")
+            check_iface2 pileft
+            ;;
+        "piright-1")
+            check_iface1 piright
+            ;;
+        "piright-2")
+            check_iface2 piright
+            ;;
+        "dmesg")
+            check_dmesg
+            ;;
+    esac
+
+    check_return "${test_case_id}"
+}
+
+# Test run.
+create_out_dir "${OUTPUT}"
+
+if [ "${SKIP_INSTALL}" = "true" ] || [ "${SKIP_INSTALL}" = "True" ]; then
+    info_msg "Package installation skipped"
+else
+    install
+fi
+
+for t in $TESTS; do
+    run "$t"
+done
