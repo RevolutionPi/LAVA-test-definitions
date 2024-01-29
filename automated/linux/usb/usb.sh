87f8fdce7b30e168a18f743d6b4bb3178b1819c8
Ramiro Gsponer <r.gsponer@kunbus.com>
2024-01-29 16:36:43 +0100

    refactor(usb): Update tests to new structure
    In order to improve the usb tests, these tests are incorporated while
    maintaining the defined structure.
    
    The usb-1 test was implemented according to the previous test
    `mount.sh`.
    The usb-2 test was implemented according to the previous test
    `test-usb-2.sh`.
    The usb-4 test was implemented according to the previous test
    `test-usb-4.sh`.
    The usb-5 test was implemented according to the previous test
    `test-usb-3.sh`.
    
    usb-1: Mount USB device and check if successful
    - Create the mount point if it does not exist.
    - Attempt to mount the specified USB device to the mount point.
    - If successful, report the test case as passed. Otherwise, report failure.
    
    usb-2: Format and verify the integrity of a USB flash disk
    - If the device is found, format it using mke2fs with ext4 file system.
    - Create a mount point if it doesn't exist and mount the formatted
    device.
    - Generate a test file with random data, calculate its md5sum, and copy
    it to the mounted USB.
    - Verify md5sum integrity on the mounted USB.
    - If all steps are successful, report the test case as passed.
    Otherwise, report failure.
    
    usb-4: Measure the write speed of the USB device using dd
    - Run dd to write 100MB of zero data to the USB device.
    - Capture the output of dd to determine the write speed.
    - Check if the write speed is greater than a predefined minimum.
    - Report the test case as passed if the speed is above the threshold,
    otherwise report failure.
    
    usb-5: Measure the read speed of the USB device using dd.
    - Run dd to read 100MB of data from the USB device to /dev/null.
    - Capture the output of dd to determine the read speed.
    - Check if the read speed is greater than a predefined minimum.
    - Report the test case as passed if the speed is above the threshold,
    otherwise report failure.
    
    Test usb-3 will be implemented soon.
    
    Signed-off-by: Ramiro Gsponer <r.gsponer@kunbus.com>

---
+++ automated/linux/usb/usb.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+. ../../lib/sh-test-lib
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
+export RESULT_FILE
+SKIP_INSTALL="False"
+TESTS="usb-1 usb-2 usb-4 usb-5"
+MOUNT_POINT="/mnt/usb"
+SPEED_DEFAULT_READ_MIN=15
+SPEED_DEFAULT_WRITE_MIN=4
+
+usage() {
+    echo "Usage: $0 [-s <true|false>] [-d device] [-m mount_point] [-t test] [-r speed_default_read_min] [-w speed_default_write_min]" 1>&2
+    exit 1
+}
+
+while getopts "s:d:m:t:r:w:h" o; do
+    case "$o" in
+    s) SKIP_INSTALL="${OPTARG}" ;;
+    d) DEVICE="${OPTARG}" ;;
+    m) MOUNT_POINT="${OPTARG}" ;;
+    t) TESTS="${OPTARG}" ;;
+    r) SPEED_DEFAULT_READ_MIN="${OPTARG}" ;;
+    w) SPEED_DEFAULT_WRITE_MIN="${OPTARG}" ;;
+    h|*) usage ;;
+    esac
+done
+
+install() {
+    apt-get update -q
+    apt-get -y install bc
+}
+
+run() {
+    # shellcheck disable=SC3043
+    local test_case_id="$1"
+    # shellcheck disable=SC3043
+    local output=""
+    # shellcheck disable=SC3043
+    local speed=""
+    info_msg "Running ${test_case_id} test..."
+
+    if mount | grep -q "$DEVICE"; then
+        umount "$DEVICE"
+    fi
+    parted -s "${DEVICE%[0-9]}" mklabel msdos
+    parted -s "${DEVICE%[0-9]}" mkpart primary ext4 0% 100%
+    mkfs.ext4 -F "$DEVICE"
+
+    case "$test_case_id" in
+    "usb-1")
+        mkdir -p "$MOUNT_POINT"
+
+        if mount "$DEVICE" "$MOUNT_POINT"; then
+            report_pass "$test_case_id"
+            umount "$MOUNT_POINT"
+        else
+            error_msg "$test_case_id FAIL!"
+        fi
+        ;;
+    "usb-2")
+        if lsblk | grep "$(basename "$DEVICE")"
+        then
+            #Flash disk will be formatted
+            if mke2fs -F -t ext4 "$DEVICE"
+            then
+                if [ ! -d "$MOUNT_POINT" ]
+                then
+                    mkdir "$MOUNT_POINT"
+                fi
+
+                if mount "$DEVICE" "$MOUNT_POINT"
+                then
+                    dd if=/dev/urandom of=/tmp/testfile bs=4k count=256k
+                    md5sum /tmp/testfile > md5
+
+                    cp /tmp/testfile "$MOUNT_POINT"
+                    cp md5 "$MOUNT_POINT"
+
+                    if cd "$MOUNT_POINT" && md5sum -c md5
+                    then
+                        report_pass "$test_case_id md5sum"
+                    else
+                        error_msg "$test_case_id md5sum FAIL!"
+                    fi
+                else
+                    error_msg "$test_case_id mount FAIL!"
+                fi
+                cd /
+                umount "$MOUNT_POINT"
+                rm -r "$MOUNT_POINT"
+            else
+                error_msg "$test_case_id format FAIL!"
+            fi
+        else
+            error_msg "$test_case_id flash-disk FAIL!"
+        fi
+        ;;
+    "usb-4")
+        # Run dd to measure the speed - write
+        output=$(dd if=/dev/zero of="$DEVICE" bs=1k count=100000 2>&1)
+        echo "$output"
+        speed=$(echo "$output" | awk '/copied/{print $10}')
+        if [ "$(echo "$speed >= $SPEED_DEFAULT_WRITE_MIN" | bc -l)" -eq 1 ]; then
+            report_pass "Overall write speed is greater than $SPEED_DEFAULT_WRITE_MIN MB/s"
+        else
+            error_msg "Overall write speed is less than $SPEED_DEFAULT_WRITE_MIN MB/s -> FAIL!"
+        fi
+        ;;
+    "usb-5")
+        # Run dd to measure the speed - read
+        output=$(dd if="$DEVICE" of=/dev/null bs=1k count=100000 2>&1)
+        echo "$output"
+        speed=$(echo "$output" | awk '/copied/{print $10}')
+        if [ "$(echo "$speed >= $SPEED_DEFAULT_READ_MIN" | bc -l)" -eq 1 ]; then
+            report_pass "Overall read speed is greater than $SPEED_DEFAULT_READ_MIN MB/s"
+        else
+            error_msg "Overall read speed is less than $SPEED_DEFAULT_READ_MIN MB/s -> FAIL!"
+        fi
+        ;;
+    esac
+
+    check_return "${test_case_id}"
+}
+
+# Test run.
+create_out_dir "${OUTPUT}"
+
+
+if [ "${SKIP_INSTALL}" = "true" ] || [ "${SKIP_INSTALL}" = "True" ]; then
+    info_msg "Package installation skipped"
+else
+    install
+fi
+
+for t in $TESTS; do
+    run "$t"
+done
