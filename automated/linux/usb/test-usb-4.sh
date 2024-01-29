c59365b2853d7afe9cf9f0cc3ae243401efc2ccf
Ramiro Gsponer <r.gsponer@kunbus.com>
2024-01-29 16:43:14 +0100

    fix(usb): Remove tests not matching new structure
    The tests have been reimplemented in
    `automated/linux/usb/usb.sh`.
    
    Test `mount.sh` can be found as usb-1.
    Test `test-usb-2.sh`can be found as usb-2.
    Test `test-usb-3.sh`can be found as usb-5.
    Test `test-usb-4.sh`can be found as usb-4.
    
    Test usb-3 will be implemented soon.
    
    Signed-off-by: Ramiro Gsponer <r.gsponer@kunbus.com>

--- automated/linux/usb/test-usb-4.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-TEST_CASE_NAME=$(basename "$0" .sh)
-
-if dd if=/dev/zero of=/dev/sda1 status=progress conv=sync iflag=nocache oflag=nocache bs=1k count=1M
-then
-    lava-test-case "$TEST_CASE_NAME" --result pass
-else
-    lava-test-case "$TEST_CASE_NAME" --result fail
-fi
