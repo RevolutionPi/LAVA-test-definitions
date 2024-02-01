RevolutionPi/LAVA-test-definitions

commit a7a6f73d15f539801d78c99930b79d98e2ccd750

refactor(eth): Update tests to new structure
In order to improve the eth tests, these tests are incorporated while
maintaining the defined structure.
The eth-1 test was implemented according to the previous test
`test-eth-ethtool.sh`.
The test eth-3 was implemented according to the previous test
`test-eth-iperf3.sh`.

The eth-1 test retains its original form, where it checks for the
existence of certain parameters after executing the `ethtool` command.
The eth-3 test has been slightly modified to check the performance of
the interface. The check is performed using the output of the `iperf3`
command in JSON format.

Signed-off-by: Ramiro Gsponer <r.gsponer@kunbus.com>

automated/lib/eth.sh
@@ -1,0 +1,14 @@
+#!/bin/sh
+
+# Waiting for Parameter:
+#1- Speed: 100Mb/s
+#2- Duplex: Full
+#3- Port: Twisted Pair
+#4- Auto-negotiation: on
+#5- Link detected: yes
+
+ETH_PARAM="Speed:\s*100
+Duplex:\s*Full
+Port:\s*Twisted\s+Pair
+Auto-negotiation:\s*on
+Link\s+detected:\s*yes"

automated/linux/ethernet/eth.sh
@@ -1,0 +1,122 @@
+#!/bin/sh
+
+# shellcheck disable=SC1091
+. ../../lib/sh-test-lib
+. ../../lib/eth.sh
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
+export RESULT_FILE
+DUT=""
+IP_ATE="10.42.11.150"
+SKIP_INSTALL="True"
+TESTS="eth-1 eth-3"
+BITRATE_DEFAULT=90
+
+
+usage() {
+    echo "Usage: $0 [-s <true|false>] [-d dut] [-i ip_ate] [-t tests] [-b expected_bitrate]" 1>&2
+    exit 1
+}
+
+while getopts "d:s:i:t:b:B:h" o; do
+  case "$o" in
+  d) DUT="${OPTARG}" ;;
+  s) SKIP_INSTALL="${OPTARG}" ;;
+  i) IP_ATE="${OPTARG}" ;;
+  t) TESTS="${OPTARG}" ;;
+  b) BITRATE_DEFAULT="${OPTARG}" ;;
+  h|*) usage ;;
+  esac
+done
+
+install() {
+    apt-get update -q
+    apt-get -y install iperf3 jq
+}
+
+check_ethtool() {
+    # shellcheck disable=SC3043
+    local interface="$1"
+    # shellcheck disable=SC3043
+    local ret_ethtool=0
+    ret_ethtool=$(ethtool "$interface")
+    echo "$ret_ethtool"
+     # Iterate over the positional parameters
+    for param in $ETH_PARAM; do
+        if echo "$ret_ethtool" | grep -E -o "$param"
+        then
+            report_pass "eth-1_$interface-${param%:*}"
+        else
+            report_fail "eth-1_$interface-${param%:*}"
+        fi
+    done
+}
+
+check_iperf3() {
+    # shellcheck disable=SC3043
+    local check_nr="$1"
+    # shellcheck disable=SC3043
+    local output_iperf3=""
+    # shellcheck disable=SC3043
+    local bitrate_average=0
+    case "$check_nr" in
+    1)
+        output_iperf3=$(iperf3 -t 1800 -4 -c "$IP_ATE" -t 10 -J)
+        echo "$output_iperf3"
+        bitrate_average=$(echo "$output_iperf3" | jq -r '.end.sum_sent.bits_per_second' | awk '{ printf "%.2f", $1 / 1000000 }')
+        ;;
+    2)
+        output_iperf3=$(iperf3 -t 1800 -4 -R -c "$IP_ATE" -t 10 -J)
+        echo "$output_iperf3"
+        bitrate_average=$(echo "$output_iperf3" | jq -r '.end.sum_received.bits_per_second' | awk '{ printf "%.2f", $1 / 1000000 }')
+        ;;
+    *)
+        error_msg "Undefined test..."
+    esac
+
+    if [ "$(printf "%.0f" "$bitrate_average")" -gt "$BITRATE_DEFAULT" ]; then
+        info_msg "Bitrate average: $bitrate_average Mbit/s - Bitrate expected: $BITRATE_DEFAULT Mbit/s -> eth-3_$check_nr/2 OK"
+    else
+        error_msg "Bitrate average: $bitrate_average Mbit/s - Bitrate expected: $BITRATE_DEFAULT Mbit/s -> eth-3_$check_nr/2 FAIL"
+    fi
+}
+
+run() {
+    # shellcheck disable=SC3043
+    local test_case_id="$1"
+    info_msg "Running ${test_case_id} test..."
+
+    case "$test_case_id" in
+    "eth-1")
+        check_ethtool eth0
+        case "$DUT" in
+        RevPi_Connect)
+            check_ethtool eth1
+            ;;
+        *)
+            ;;
+        esac
+        ;;
+    "eth-3")
+        output=$(ip a show eth0 | grep inet)
+        info_msg "$output"
+        check_iperf3 1
+        check_iperf3 2
+        ;;
+    esac
+
+    check_return "${test_case_id}"
+}
+
+# Test run.
+create_out_dir "${OUTPUT}"
+
+if [ "${SKIP_INSTALL}" = "true" ] || [ "${SKIP_INSTALL}" = "True" ]; then
+    info_msg "Package installation skipped"
+else
+    install
+fi
+
+for t in $TESTS; do
+    run "$t"
+done

automated/linux/ethernet/eth.yaml
@@ -1,0 +1,37 @@
+---
+metadata:
+  name: Tests eth
+  format: "Lava-Test Test Definition 1.0"
+  description: "Test for eth interfaces"
+  maintainer:
+    - r.gsponer@kunbus.com
+  os:
+    - debian
+  scope:
+    - functional
+  devices:
+    - RevPi Core
+    - RevPi Core 3
+    - RevPi Core 3+
+    - RevPi Core S
+    - RevPi Core SE
+    - RevPi Connect
+    - RevPi Connect+
+    - RevPi Connect S
+    - RevPi Connect SE
+    - RevPi Connect 4
+    - RevPi Compact
+    - RevPi Flat
+
+params:
+  SKIP_INSTALL: "False"
+  DUT: ""
+  IP_ATE: "10.42.11.150"
+  TESTS: "eth-1 eth-3"
+  BITRATE_DEFAULT: 90
+
+run:
+  steps:
+    - cd automated/linux/ethernet
+    - ./eth.sh -s "${SKIP_INSTALL}" -d "${DUT}" -i "${IP_ATE}" -t "${TESTS}" -b "${BITRATE_DEFAULT}"
+    - ../../utils/send-to-lava.sh ./output/result.txt

commit 7453f06cd3a000c280f0ed6d3427a6b24c71c851

fix: export ETH_PARAM for use
Fix SC2034 by exporting ETH_PARAM.

Fixes: a7a6f73 ("refactor(eth): Update tests to new structure")
Link: https://www.shellcheck.net/wiki/SC2034
Signed-off-by: Thomas Böhler <t.boehler@kunbus.com>
Reviewed-by: Ramiro Gsponer <r.gsponer@kunbus.com>
Acked-by: Ramiro Gsponer <r.gsponer@kunbus.com>

automated/lib/eth.sh
@@ -7,7 +7,7 @@
 #4- Auto-negotiation: on
 #5- Link detected: yes
 
-ETH_PARAM="Speed:\s*100
+export ETH_PARAM="Speed:\s*100
 Duplex:\s*Full
 Port:\s*Twisted\s+Pair
 Auto-negotiation:\s*on