RevolutionPi/LAVA-test-definitions

commit 3b13138692cffa5265d40e10f68135dbfe9c05ae

fix(piControl): Improve test to handle errors
piControl test script has been improved to include additional checks on
test cases 'pc-1' and 'pc-2'.

The script now uses different log levels when examining the output of
'dmesg' for specific patterns related to piControl issues.

If errors are detected, an error message is displayed, aiding in the
identification and debugging of piControl-related issues.

In the 'pc-2' test case, the script has been modified to check for the
existence of files in the '/dev/' directory matching the 'piControl*'
pattern, ensuring a more robust validation approach.

Signed-off-by: Ramiro Gsponer <r.gsponer@kunbus.com>

automated/revpi/piControl/piControl.sh
@@ -28,21 +28,42 @@ install() {
     apt-get -y install coreutils
 }
 
+check_dmesg() {
+    # shellcheck disable=SC3043
+    local log_level="$1"
+    # shellcheck disable=SC3043
+    local param_grep="$2"
+    # shellcheck disable=SC3043
+    local dmesg_output=""
+    dmesg_output="$(dmesg -l "$log_level" | grep -E "$param_grep")"
+    if [ -n "$dmesg_output" ]; then
+        info_msg "Something went wrong..."
+        info_msg "log_level: $log_level"
+        info_msg "param_grep: $param_grep"
+        echo "$dmesg_output"
+        error_msg "piControl error(s) occured. Check output of warning messages above."
+    fi
+}
+
 run() {
     # shellcheck disable=SC3043
-    local test="$1"
-    test_case_id="${test}"
-    echo
+    local test_case_id="$1"
     info_msg "Running ${test_case_id} test..."
 
-    case "$test" in
+    case "$test_case_id" in
       "pc-1")
-          info_msg "Image test: pc-1"
+          info_msg "Output piControl in dmesg"
           dmesg | grep piControl
+
+          check_dmesg "emerg,alert,crit,err,warn" "piControl"
+          # Catch errors or failures from other levels
+          check_dmesg "notice,info,debug" "piControl.*fail|piControl.*err|piControl.*incorrect"
           ;;
       "pc-2")
           info_msg "Image test: pc-2"
-          ls /dev/piControl*
+          if [ -e "/dev/piControl*" ]; then
+            error_msg "pc-2 failed: /dev/piControl* doesn't exist"
+          fi
           ;;
     esac
 

commit 96ab43339d0d5678a09a09325a9c01bb31d3a876

fix(piControl): Fix test output if error occurs
If an error occurs during the test, the error text is displayed in the
output, but LAVA does not mark it as an error. This commit fixes this.

Fixes: 3b13138 ("fix(piControl): Improve test to handle errors")
Signed-off-by: Ramiro Gsponer <r.gsponer@kunbus.com>

automated/revpi/piControl/piControl.sh
@@ -41,7 +41,8 @@ check_dmesg() {
         info_msg "log_level: $log_level"
         info_msg "param_grep: $param_grep"
         echo "$dmesg_output"
-        error_msg "piControl error(s) occured. Check output of warning messages above."
+        report_fail "piControl error(s) occured. Check output of warning messages above."
+        return 1
     fi
 }
 
@@ -56,13 +57,14 @@ run() {
           dmesg | grep piControl
 
           check_dmesg "emerg,alert,crit,err,warn" "piControl"
+          check_return "${test_case_id}"
           # Catch errors or failures from other levels
           check_dmesg "notice,info,debug" "piControl.*fail|piControl.*err|piControl.*incorrect"
           ;;
       "pc-2")
           info_msg "Image test: pc-2"
           if [ -e "/dev/piControl*" ]; then
-            error_msg "pc-2 failed: /dev/piControl* doesn't exist"
+            report_fail "pc-2 failed: /dev/piControl* doesn't exist"
           fi
           ;;
     esac