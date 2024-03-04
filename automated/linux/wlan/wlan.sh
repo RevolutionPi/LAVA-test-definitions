99d7e9a257176c2df383b721c2843b58c0e69ea7
Ramiro Gsponer <r.gsponer@kunbus.com>
2024-03-04 13:16:20 +0100

    feat: Add WLAN tests
    Added tests for WLAN functionality using Network Manager.
    This includes tests for configuring WLAN, scanning for networks,
    connecting to a network, and disconnecting from a network.
    
    The script allows specifying custom WLAN interface, WLAN SSID, WLAN
    password, and WLAN sleep time through command line options.
    The default WLAN interface is set to wlan0.
    If WLAN SSID and WLAN password are not provided, the script will display
    usage instructions.
    
    Options:
    -t: Specify specific tests to run.
    -I: Specify the WLAN interface to use (default: wlan0).
    -S: Specify the sleep time for WLAN operations (default: 10 seconds).
    -W: WLAN SSID to connect.
    -P: WLAN password.
    
    Tests:
    wlan-config-nm: Configuring WLAN using Network Manager.
    wlan-1-nm: Scanning for available WLAN networks using Network Manager.
    wlan-2-nm-a: Connecting to a WLAN network using Network Manager.
    wlan-2-nm-b: Showing connection details of a WLAN network using Network
     Manager.
    wlan-2-nm-disconnect: Disconnecting from a WLAN network using Network
     Manager.
    
    On RevPi Connect 4 devices the "wlan-enable-ext-antenna" test must be
    the first test performed in order to be able to use 2.4GHz networks.
    After enabling the external antenna in "wlan-enable-ext-antenna", the
    device under test (DUT) will reboot automatically after 1 minute.
    If the DUT performs a reboot, the Job will be marked as FAIL, as the
    connection will be lost.
    This should be handled by the tester in the same way as with RTC tests,
    i.e. a new deploy, boot and test section should be generated and used
    for subsequent tests.
    
    Signed-off-by: Ramiro Gsponer <r.gsponer@kunbus.com>

---
+++ automated/linux/wlan/wlan.sh
@@ -0,0 +1,92 @@
+#!/bin/sh
+
+# shellcheck disable=SC1091
+. ../../lib/sh-test-lib
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
+export RESULT_FILE
+
+TESTS="wlan-config-nm wlan-1-nm wlan-2-nm-a wlan-sleep wlan-2-nm-b wlan-2-nm-disconnect"
+WLAN_INTERFACE="wlan0"
+WLAN_SSID=""
+WLAN_PASSWORD=""
+WLAN_SLEEP=10
+
+usage() {
+    echo "Usage: $0 [-t tests] [-I wlan_interface] [-S wlan_sleep] -W <wlan_ssid> -P <wlan_password>" 1>&2
+    exit 1
+}
+
+while getopts "t:I:S:W:P:h" o; do
+    case "$o" in
+    t) TESTS="${OPTARG}" ;;
+    I) WLAN_INTERFACE="${OPTARG}" ;;
+    S) WLAN_SLEEP="${OPTARG}" ;;
+    W) WLAN_SSID="${OPTARG}" ;;
+    P) WLAN_PASSWORD="${OPTARG}" ;;
+    h|*) usage ;;
+    esac
+done
+
+run() {
+    local test_case_id="$1"
+    local output=""
+    info_msg "Running ${test_case_id} test..."
+
+    case "${test_case_id}" in
+    "wlan-enable-ext-antenna")
+        output="$(revpi-config enable external-antenna)"
+        [ "${output}" -ne 0 ] || shutdown -r +1
+        ;;
+    "wlan-config-nm")
+        echo "country=DE" >> /etc/NetworkManager/NetworkManager.conf
+        systemctl restart NetworkManager
+        # Wait a few seconds after configuration
+        sleep 5
+        ;;
+    "wlan-1-nm")
+        nmcli -c no device wifi rescan ifname "${WLAN_INTERFACE}"
+        sleep 5
+        nmcli -f SSID -c no device wifi list ifname "${WLAN_INTERFACE}" | grep "${WLAN_SSID}"
+        ;;
+    "wlan-2-nm-a")
+        nmcli device wifi connect "${WLAN_SSID}" password "${WLAN_PASSWORD}" name TEST
+        output="$?"
+        if [ "${output}" -ne 0 ]; then
+            error_msg "Unable to connect to SSID: ${WLAN_SSID}. Test aborted"
+        fi
+        ;;
+    "wlan-2-nm-b")
+        output="$(nmcli -t connection show TEST)"
+        if ! echo "${output}" | grep -q "802-11-wireless.ssid:${WLAN_SSID}"; then
+            report_fail "802-11-wireless.ssid should be ${WLAN_SSID}"
+        fi
+        if ! echo "${output}" | grep -q "802-11-wireless.mode:infrastructure"; then
+            report_fail "802-11-wireless.mode NOT infrastructure!"
+        fi
+        ;;
+    "wlan-2-nm-disconnect")
+        nmcli connection delete TEST
+        ;;
+    "wlan-sleep")
+        sleep "${WLAN_SLEEP}";
+        ;;
+    *)
+        report_fail "Undefined test..."
+        ;;
+    esac
+
+    check_return "${test_case_id}"
+}
+
+# Test run.
+create_out_dir "${OUTPUT}"
+
+if [ -z "$WLAN_SSID" ] || [ -z "$WLAN_PASSWORD" ]; then
+  echo "Options -W and -P are mandatory." >&2
+  usage
+fi
+
+for t in $TESTS; do
+    run "$t"
+done
