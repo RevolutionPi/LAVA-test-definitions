c6419edfa95dc27d74867911bab46577e2894c84
Thomas Böhler <t.boehler@kunbus.com>
2024-06-13 09:50:17 +0200

    feat: Allow custom IO tests in pt.sh
    Allow testing custom IOs. This is done by giving the DIGITAL_IOS and
    ANALOG_IOS variables and running the tests pt_test_digital_ios and
    pt_test_analog_ios respectively.
    
    Signed-off-by: Thomas Böhler <t.boehler@kunbus.com>

--- automated/revpi/piTest/test-wrapper.sh
+++ automated/revpi/piTest/test-wrapper.sh
@@ -7,16 +7,20 @@ OUTPUT="$(pwd)/output"
 RESULT_FILE="${OUTPUT}/result.txt"
 export RESULT_FILE
 TESTS="pt-1"
+DIGITAL_IOS=""
+ANALOG_IOS=""
 
 usage() {
     echo "Usage: $0 [-s <true|false>] [-t test]" 1>&2
     exit 1
 }
 
-while getopts "t:s:h" o; do
+while getopts "t:s:d:a:h" o; do
     case "$o" in
         t) TESTS="${OPTARG}" ;;
         s) SKIP_INSTALL="${OPTARG}" ;;
+        d) DIGITAL_IOS="${OPTARG}" ;;
+        a) ANALOG_IOS="${OPTARG}" ;;
         h|*) usage ;;
     esac
 done
@@ -28,6 +32,63 @@ install() {
 
 pt_1() {
     piTest_Check_config "test-pt1-pt2" "$(piTest -d)"
+}
+
+pt_test_digital_ios() {
+    local ios="$1"
+    local input
+    local output
+    local power
+
+    # TODO: skipping this is still reported as pass in the calling context
+
+    if [ -z "$ios" ]; then
+        info_msg "No digital IOs defined. Skipping test."
+        report_skip "test-digital-ios"
+
+        return 0
+    fi
+
+    # substitute the ';' to '\n' to not have to set IFS and potentially break
+    # other loops called later
+    ios="$(echo "$ios" | tr ';' '\n')"
+    for line in $ios; do
+        input="$(echo "$line" | cut -d',' -f1)"
+        output="$(echo "$line" | cut -d',' -f2)"
+        power="$(echo "$line" | cut -d',' -f3)"
+
+        # turn on power before testing
+        if [ -n "$power" ]; then
+            piTest_setIOValue "test-$input-$output-power-on" "$power" "$HIGH"
+        fi
+
+        piTest_Check_001 "test-$input-$output" "$input" "$output"
+
+        if [ -n "$power" ]; then
+            piTest_setIOValue "test-$input-$output-power-off" "$power" "$LOW"
+        fi
+    done
+}
+
+pt_test_analog_ios() {
+    local ios="$1"
+    local input
+    local output
+
+    if [ -z "$ios" ]; then
+        info_msg "No analog IOs defined. Skipping test."
+        report_skip "test-analog-ios"
+
+        return 0
+    fi
+
+    ios="$(echo "$ios" | tr ';' '\n')"
+    for line in $ios; do
+        input="$(echo "$line" | cut -d',' -f1)"
+        output="$(echo "$line" | cut -d',' -f2)"
+
+        piTest_Check_002 "test-$input-$output" "$input" "$output"
+    done
 }
 
 test_pt_compact_d_1() {
@@ -75,6 +136,13 @@ run() {
     case "$test_case_id" in
         "pt-1")
             pt_1
+            ;;
+        "pt_test_digital_ios")
+            pt_test_digital_ios "$DIGITAL_IOS"
+            ;;
+
+        "pt_test_analog_ios")
+            pt_test_analog_ios "$ANALOG_IOS"
             ;;
         "test_pt_compact_d_1")
             test_pt_compact_d_1
