d9c3c48a5c2aa58f3fe91ec58afb76a09f33d5cb
Thomas Böhler <t.boehler@kunbus.com>
2024-06-13 10:51:56 +0200

    refactor: Update config-loader to new structure
    Signed-off-by: Thomas Böhler <t.boehler@kunbus.com>

--- device-config/config_loader.sh
+++ device-config/config_loader.sh
@@ -1,28 +1,77 @@
-#!/bin/bash
+#!/bin/sh
+
+# shellcheck disable=SC1091
+. ../lib/sh-test-lib
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
+export RESULT_FILE
+SKIP_INSTALL="false"
 
 TEST_CASE_NAME=$(basename "$0" .sh)
-
 CONFIG_PATH="/var/www/revpi/pictory/projects"
+
+usage() {
+    cat >&2 << EOF
+Usage: $0 [-s SKIP_INSTALL] CONFIG
+
+Install a Revolution Pi config from path "CONFIG" to "$CONFIG_PATH" and reload
+piControl.
+EOF
+    exit 1
+}
+
+install() {
+    apt-get update -q
+    apt-get -qy install pitest
+}
+
+run() {
+    local config="$1"
+    local config_path="$2"
+
+    if ! cp "$config" "$config_path"/_config.rsc
+    then
+        # fail the complete job if setting up the configuration fails
+        # if this fails the following jobs most likely fail, too
+        error_fatal "$TEST_CASE_NAME-install-config"
+    else
+        report_pass "$TEST_CASE_NAME-install-config"
+    fi
+
+    chown www-data:www-data "$config_path"/_config.rsc
+
+    if ! piTest -x
+    then
+        error_fatal "$TEST_CASE_NAME-reload-driver"
+    else
+        report_pass "$TEST_CASE_NAME-reload-driver"
+    fi
+
+    # wait for piTest configuration to be loaded
+    sleep 1
+}
+
+while getopts "s:h" o; do
+    case "$o" in
+    s) SKIP_INSTALL="$OPTARG" ;;
+    h|*) usage ;;
+    esac
+done
+
+shift $((OPTIND - 1))
 
 CONFIG="$1"
 
-if ! cp "$CONFIG" "$CONFIG_PATH"/_config.rsc
-then
-    # fail the complete job if setting up the configuration fails
-    # if this fails the following jobs most likely fail, too
-    lava-test-raise "$TEST_CASE_NAME-install-config"
-else
-    lava-test-case "$TEST_CASE_NAME-install-config" --result pass
+if [ -z "$CONFIG" ]; then
+    usage
 fi
 
-chown www-data:www-data "$CONFIG_PATH"/_config.rsc
-
-if ! piTest -x
-then
-    lava-test-raise "$TEST_CASE_NAME-reload-driver"
+if [ "$SKIP_INSTALL" = "true" ] || [ "$SKIP_INSTALL" = "True" ]; then
+    install
 else
-    lava-test-case "$TEST_CASE_NAME-reload-driver" --result pass
+    info_msg "Package installation skipped"
 fi
 
-# wait for piTest configuration to be loaded
-sleep 1
+create_out_dir "$OUTPUT"
+
+run "$CONFIG" "$CONFIG_PATH"
