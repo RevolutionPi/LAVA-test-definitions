RevolutionPi/LAVA-test-definitions

commit a375e8be7fdf4071afdb4f40547582c553836efd

fix(usb): Only format if needed
Signed-off-by: Thomas Böhler <t.boehler@kunbus.com>

automated/linux/usb/usb.sh
@@ -34,6 +34,22 @@ install() {
     apt-get -y install bc fdisk
 }
 
+check_available_dev() {
+    if [ ! -b "$1" ];then
+        error_msg "Block device $1 not found"
+    fi
+}
+
+# usage: ensure_mountable_partition DEVICE PARTITION
+ensure_mountable_partition() {
+	check_available_dev "$1"
+
+	if [ ! -b "$2" ] || ! (file -Ls "$2" | grep -q ext4); then
+        partition_disk "${DEVICE%[0-9]}"
+        format_partitions "${DEVICE%[0-9]}" ext4
+	fi
+}
+
 run() {
     local test_case_id="$1"
     local output=""
@@ -45,8 +61,7 @@ run() {
         exit_on_fail "$test_case_id-umount-left-over-mount"
     fi
 
-    partition_disk "${DEVICE%[0-9]}"
-    format_partitions "${DEVICE%[0-9]}" ext4
+    ensure_mountable_partition "${DEVICE%[0-9]}" "$DEVICE"
 
     case "$test_case_id" in
     "usb-1")

commit 32df13e69f4bff70238c47f423f1d1b33c82b5c7

style(usb): Use spaces for indentation
Fixes: a375e8b ("fix(usb): Only format if needed")
Signed-off-by: Thomas Böhler <t.boehler@kunbus.com>

automated/linux/usb/usb.sh
@@ -42,12 +42,12 @@ check_available_dev() {
 
 # usage: ensure_mountable_partition DEVICE PARTITION
 ensure_mountable_partition() {
-	check_available_dev "$1"
+    check_available_dev "$1"
 
-	if [ ! -b "$2" ] || ! (file -Ls "$2" | grep -q ext4); then
+    if [ ! -b "$2" ] || ! (file -Ls "$2" | grep -q ext4); then
         partition_disk "${DEVICE%[0-9]}"
         format_partitions "${DEVICE%[0-9]}" ext4
-	fi
+    fi
 }
 
 run() {