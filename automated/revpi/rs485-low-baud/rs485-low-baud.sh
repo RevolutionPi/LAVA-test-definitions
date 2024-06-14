ea914110e758b478631c6f14e7f5c91999f64de8
Thomas Böhler <t.boehler@kunbus.com>
2024-06-14 09:20:53 +0200

    feat: Add rs485-low-baud test
    Signed-off-by: Thomas Böhler <t.boehler@kunbus.com>

---
+++ automated/revpi/rs485-low-baud/rs485-low-baud.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+# shellcheck disable=SC1091
+. ../../lib/sh-test-lib
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
+export RESULT_FILE
+SKIP_INSTALL="false"
+TESTS="rs485-low-baud"
+RSDEV="/dev/ttyRS485"
+
+usage() {
+    cat << EOF
+Usage: $0 [-s SKIP_INSTALL] [-t TESTS] [-d RSDEV]
+EOF
+
+    exit "$1"
+}
+
+install() {
+    :
+}
+
+run() {
+    local test_case_id="$1"
+    info_msg "Running $test_case_id test..."
+
+    case "$test_case_id" in
+    "rs485-low-baud")
+        local previous_baud
+        local errors
+        previous_baud="$(stty -F "$RSDEV" -echo raw speed 1200)"
+        exit_on_fail "$test_case_id-set-baud"
+
+        dmesg_capture_start
+
+        for _i in $(seq 1 100); do
+            echo "test" > "$RSDEV" || error_msg "Failure sending message on $RSDEV"
+        done
+
+        errors="$(dmesg_capture_result | grep "piControl")"
+
+        if [ -n "$errors" ]; then
+            echo "$errors"
+            error_msg "Errors occured in piControl during rs485 low baud test"
+        else
+            report_pass "$test_case_id"
+        fi
+
+        stty -F "$RSDEV" -echo raw speed "$previous_baud" > /dev/null
+        check_return "$test_case_id-set-previous-baud"
+        ;;
+    *) error_msg "Unknown test $test_case_id" ;;
+    esac
+}
+
+while getopts "t:d:s:h" o; do
+    case "$o" in
+    t) TESTS="$OPTARG";;
+    d) RSDEV="$OPTARG" ;;
+    s) SKIP_INSTALL="$OPTARG" ;;
+    h) usage 0 ;;
+    '?') usage "1" >&2 ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+create_out_dir "$OUTPUT"
+
+if [ "${SKIP_INSTALL}" = "true" ] || [ "${SKIP_INSTALL}" = "True" ]; then
+    info_msg "Package installation skipped"
+else
+    install
+fi
+
+for t in $TESTS; do
+    run "$t"
+done
