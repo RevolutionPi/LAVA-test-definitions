f1ca5f1ccdcfe35aeb1a8dde7e44bf6b1d6bccb0
Thomas Böhler <t.boehler@kunbus.com>
2024-06-18 16:07:15 +0200

    feat(piControl): Add error line count measurement
    Add the amount of error lines found in dmesg as a measurement to the
    result.
    
    Signed-off-by: Thomas Böhler <t.boehler@kunbus.com>

--- automated/revpi/piControl/piControl.sh
+++ automated/revpi/piControl/piControl.sh
@@ -31,16 +31,19 @@ check_dmesg() {
     local log_level="$1"
     local param_grep="$2"
     local dmesg_output=""
+
     dmesg_output="$(dmesg -l "$log_level" \
         | grep -E "$param_grep" \
         | grep -v "loading out-of-tree module taints kernel.")"
+
     if [ -n "$dmesg_output" ]; then
         info_msg "Something went wrong..."
         info_msg "log_level: $log_level"
         info_msg "param_grep: $param_grep"
         echo "$dmesg_output"
         warn_msg "piControl error(s) occured. Check output of warning messages above."
-        return 1
+
+        return "$(echo "$dmesg_output" | wc -l)"
     fi
 }
 
@@ -50,22 +53,39 @@ run() {
 
     case "$test_case_id" in
       "pc-1")
+          local errors
+          local res
+
           info_msg "Output piControl in dmesg"
           dmesg | grep piControl
 
           check_dmesg "emerg,alert,crit,err,warn" "piControl"
-          check_return "${test_case_id}-errors"
+          errors=$?
+          if [ $errors -gt 0 ]; then
+              res=fail
+          else
+              res=pass
+          fi
+          add_metric "$test_case_id-errors" "$res" "$errors" lines
+
           # Catch errors or failures from other levels
           check_dmesg "notice,info,debug" "piControl.*fail|piControl.*err|piControl.*incorrect"
+          errors=$?
+          if [ $errors -gt 0 ]; then
+              res=fail
+          else
+              res=pass
+          fi
+          add_metric "$test_case_id-missed-errors" "$res" "$errors" lines
           ;;
       "pc-2")
           if [ -e "$PICONTROL_DEV" ]; then
+              report_pass "$test_case_id"
+          else
               report_fail "$test_case_id failed: $PICONTROL_DEV doesn't exist"
           fi
           ;;
     esac
-
-    check_return "${test_case_id}"
 }
 
 # Test run.
