RevolutionPi/LAVA-test-definitions

commit 0b41ba3013c7995a74817fcde8cd7e93f6488ba9

feat(piControl): Add test for permissions
Starting with picontrol-dkms 2.1.3-1+revpi12+2 "/dev/piControl0" should
always be restricted to permissions 660 and the group "picontrol".

This test is especially useful when picontrol-dkms will be replaced by
another package later to ensure the same behaviour as before.

Signed-off-by: Thomas Böhler <witcher@wiredspace.de>
Signed-off-by: Thomas Böhler <t.boehler@kunbus.com>

automated/revpi/piControl/piControl.sh
@@ -5,7 +5,7 @@
 OUTPUT="$(pwd)/output"
 RESULT_FILE="${OUTPUT}/result.txt"
 export RESULT_FILE
-TESTS="pc-1 pc-2"
+TESTS="pc-1 pc-2 pc-perms"
 SKIP_INSTALL=false
 PICONTROL_DEV="/dev/piControl0"
 
@@ -84,6 +84,13 @@ run() {
           else
               report_fail "$test_case_id failed: $PICONTROL_DEV doesn't exist"
           fi
+          ;;
+      "pc-perms")
+          [ "$(stat -c "%a" "$PICONTROL_DEV")" = "660" ]
+          check_return "$test_case_id-picontrol-dev-permissions-660"
+
+          [ "$(stat -c "%G" "$PICONTROL_DEV")" = "picontrol" ]
+          check_return "$test_case_id-picontrol-dev-group-picontrol"
           ;;
     esac
 }

automated/revpi/piControl/piControl.yaml
@@ -26,7 +26,7 @@ metadata:
 
 params:
   SKIP_INSTALL: "False"
-  TESTS: "pc-1 pc-2"
+  TESTS: "pc-1 pc-2 pc-perms"
 
 run:
   steps:

commit b207607531739efe3f089f40a8fe623aab453d09

fix(piControl): don't check perms before bookworm
Permissions for "/dev/piControl0" have only been introduced in bookworm.
Skip the tests for previous releases.

Fixes: 0b41ba3 ("feat(piControl): Add test for permissions")
Signed-off-by: Thomas Böhler <t.boehler@kunbus.com>

automated/revpi/piControl/piControl.sh
@@ -86,6 +86,16 @@ run() {
           fi
           ;;
       "pc-perms")
+          # permissions for /dev/piControl0 have only been introduced in
+          # bookworm. skip this test for all versions before bookworm.
+          . /etc/os-release
+          if [ "$VERSION_ID" -lt 12 ]; then
+              info_msg "Release is $VERSION (<12), skipping $test_case_id"
+              report_skip "$test_case_id-picontrol-dev-permissions-660"
+              report_skip "$test_case_id-picontrol-dev-group-picontrol"
+              return
+          fi
+
           [ "$(stat -c "%a" "$PICONTROL_DEV")" = "660" ]
           check_return "$test_case_id-picontrol-dev-permissions-660"
 