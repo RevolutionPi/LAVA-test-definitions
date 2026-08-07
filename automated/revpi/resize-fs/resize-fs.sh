RevolutionPi/LAVA-test-definitions

commit 5372995bb5d2b529d081c4ba3bb9dab121f51e65

fix(resize-fs): Stop after prerequisite failures
Missing block information previously allowed the test to continue with
empty values and potentially report misleading results. Return immediately
and select partition names using version ordering.

Signed-off-by: Nicolai Buchwitz <n.buchwitz@kunbus.com>

automated/revpi/resize-fs/resize-fs.sh
@@ -24,22 +24,27 @@ last_partition_resize_check() {
     if ! blockdev_size="$(cat /sys/block/"$blockdev"/size)"; then
         printf "Unable to read size of blockdev %s\n" "$blockdev" >&2
         report_fail "$test_case_id"
+        return 1
     fi
 
-	# shellcheck disable=SC2012
-    if ! last_partition="$(cd /dev && ls -1 "$blockdev"p* | tail -1)"; then
+    last_partition="$(find "/sys/block/$blockdev" -maxdepth 1 \
+        -name "${blockdev}p*" -printf '%f\n' | sort -V | tail -1)"
+    if [ -z "$last_partition" ]; then
         printf "Unable to find last partition of blockdev %s\n" "$blockdev" >&2
         report_fail "$test_case_id"
+        return 1
     fi
 
     if ! last_partition_start="$(cat /sys/block/"$blockdev"/"$last_partition"/start)"; then
         printf "Unable to get start of partition %s\n" "$last_partition" >&2
         report_fail "$test_case_id"
+        return 1
     fi
 
     if ! last_partition_size="$(cat /sys/block/"$blockdev"/"$last_partition"/size)"; then
         printf "Unable to get size of partition %s\n" "$last_partition" >&2
         report_fail "$test_case_id"
+        return 1
     fi
 
     local last_partition_end="$((last_partition_start + last_partition_size))"

commit 7c9df94ef7773aafccf092ba188336ffbf88b422

fix(resize-fs): make last partition findable
Commit 5372995b ("fix(resize-fs): Stop after prerequisite failures")
introduced finding the last partition through sysfs instead of digging
around in /dev. While this is the correct idea, it broke the test
because the entries in "/sys/block/" are symlinks to other directories,
and "find" doesn't look into them if the symlink isn't dereferenced by a
trailing forward slash.

Add the trailing forward slash so that find looks *inside* the symlink
instead of just at it, listing the blockdevice partitions properly.

Fixes: 5372995b ("fix(resize-fs): Stop after prerequisite failures")
Signed-off-by: Thomas Böhler <t.boehler@kunbus.com>

automated/revpi/resize-fs/resize-fs.sh
@@ -27,7 +27,7 @@ last_partition_resize_check() {
         return 1
     fi
 
-    last_partition="$(find "/sys/block/$blockdev" -maxdepth 1 \
+    last_partition="$(find "/sys/block/$blockdev/" -maxdepth 1 \
         -name "${blockdev}p*" -printf '%f\n' | sort -V | tail -1)"
     if [ -z "$last_partition" ]; then
         printf "Unable to find last partition of blockdev %s\n" "$blockdev" >&2