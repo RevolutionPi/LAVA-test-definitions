d5e46e966dfff0f047815ec6e2c47fe6a764d1f3
Ramiro Gsponer <r.gsponer@kunbus.com>
2026-08-08 08:57:39 +0200

    refactor(pb): use check_io/check_aio helpers
    Replace piTest_Check_001 with check_io, check_aio and check_aio_range
    in the pibridge test-wrapper.
    
    Signed-off-by: Ramiro Gsponer <r.gsponer@kunbus.com>

--- automated/revpi/pibridge/test-wrapper.sh
+++ automated/revpi/pibridge/test-wrapper.sh
@@ -5,65 +5,52 @@
 # shellcheck disable=SC1091
 source ../../lib/piTest.sh
 
-piTest_Check_001() (
-    # $1: TEST_CASE_NAME
-    # $2: INPUT
-    # $3: OUTPUT
-    test_case_name=$1
-    input=$2
-    output=$3
-
-    # set output to low
+check_io() {
+    local test_name="$1" output="$2" input="$3"
     if ! piTest_setIOValue "$output" "$LOW"; then
-        report_fail "$test_case_name-$input-low"
+        report_fail "$test_name"
         return 1
     fi
-    # wait for process image
-    sleep "$PROCIMG_WAIT"
-    if piTest_validateIOValue "$input" "$LOW"; then
-        report_pass "$test_case_name-$input-low"
-    else
-        report_fail "$test_case_name-$input-low"
+    if ! piTest_validateIOValue "$input" "$LOW"; then
+        report_fail "$test_name"
+        return 1
     fi
-
-    # set output to high
     if ! piTest_setIOValue "$output" "$HIGH"; then
-        report_fail "$test_case_name-$input-high"
+        report_fail "$test_name"
         return 1
     fi
-    # wait for process image
-    sleep "$PROCIMG_WAIT"
-    if piTest_validateIOValue "$input" "$HIGH"; then
-        report_pass "$test_case_name-$input-high"
-    else
-        report_fail "$test_case_name-$input-high"
-    fi
-)
+    piTest_validateIOValue "$input" "$HIGH"
+    check_return "$test_name"
+}
 
 test-pb-3() {
-    piTest_Check_001 "pb-3" "DIO_R1_I1" "DIO_R1_O1"
+    check_io "pb-3-DIO_R1_I1" "DIO_R1_O1" "DIO_R1_I1"
 }
 
 test-pb-4() {
-    piTest_Check_001 "pb-4" "DIO_L1_I1" "DIO_L1_O1"
+    check_io "pb-4-DIO_L1_I1" "DIO_L1_O1" "DIO_L1_I1"
 }
 
 test-pb-5() {
-    piTest_Check_001 "pb-5" "MIO_R1_DI2" "MIO_R1_DO1"
+    check_io "pb-5-MIO_R1_DI2" "MIO_R1_DO1" "MIO_R1_DI2"
 }
 
 test-pb-6() {
-    piTest_Check_001 "pb-6" "MIO_L1_DI2" "MIO_L1_DO1"
+    check_io "pb-6-MIO_L1_DI2" "MIO_L1_DO1" "MIO_L1_DI2"
 }
 
 test-pb-7() {
-    piTest_Check_001 "pb-7-mio" "MIO_L2_DI2" "MIO_L2_DO1"
-    piTest_Check_001 "pb-7-dio" "DIO_L1_I1" "DIO_L1_O1"
+    local ret=0
+    check_io "pb-7-mio-MIO_L2_DI2" "MIO_L2_DO1" "MIO_L2_DI2" || ret=$?
+    check_io "pb-7-dio-DIO_L1_I1"  "DIO_L1_O1"  "DIO_L1_I1"  || ret=$?
+    return "${ret}"
 }
 
 test-pb-8() {
-    piTest_Check_001 "pb-8-mio" "MIO_R2_DI2" "MIO_R2_DO1"
-    piTest_Check_001 "pb-8-dio" "DIO_R1_I1" "DIO_R1_O1"
+    local ret=0
+    check_io "pb-8-mio-MIO_R2_DI2" "MIO_R2_DO1" "MIO_R2_DI2" || ret=$?
+    check_io "pb-8-dio-DIO_R1_I1"  "DIO_R1_O1"  "DIO_R1_I1"  || ret=$?
+    return "${ret}"
 }
 
 if [[ $# -eq 0 ]]; then
